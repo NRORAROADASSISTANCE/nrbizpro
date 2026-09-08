b23fc660039e93c3db7b8759ab66f24f7135f838
NRORAROADASSISTANCE <abhishek.maskuri@gmail.com>
2026-09-08 16:34:30 +0530

    Add NR BizPro service worker for installable web app

---
+++ service-worker.js
@@ -0,0 +1,4 @@
+const CACHE='nr-bizpro-shell-v1';
+self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE).then(c=>c.addAll(['/','/index.html','/styles.css','/manifest.webmanifest','/pwa-icon.svg'])))});
+self.addEventListener('activate',event=>event.waitUntil(self.clients.claim()));
+self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(c=>c.put(event.request,copy));return response}).catch(()=>caches.match(event.request).then(r=>r||caches.match('/index.html'))))});

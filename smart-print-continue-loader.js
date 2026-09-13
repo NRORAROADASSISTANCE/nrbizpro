71e3efee11883528181d31b2fafd012427b1e773
NRORAROADASSISTANCE <abhishek.maskuri@gmail.com>
2026-09-13 17:14:50 +0530

    Add Smart Print Continue fix loader

---
+++ smart-print-continue-loader.js
@@ -0,0 +1,2 @@
+// Loader for the isolated Smart Print Continue fix.
+(function(){'use strict';var s=document.createElement('script');s.src='/smart-print-continue-fix-v3.js?v=20260913-1';s.async=false;document.head.appendChild(s)})();

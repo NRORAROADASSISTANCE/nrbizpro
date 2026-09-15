04f2627465d378b19288d470c213e49c71ae7758
NRORAROADASSISTANCE <abhishek.maskuri@gmail.com>
2026-09-15 20:05:02 +0530

    Bust stale PWA cache for latest billing UI

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='nr-bizpro-shell-v5';
+const CACHE='nr-bizpro-shell-v6';
 const APP_SHELL=['/','/index.html','/styles.css','/manifest.webmanifest','/pwa-icon.svg','/smart-print.html','/smart-print.css','/smart-print.js','/smart-print-manual-crop.js','/smart-print-final-override.js'];
 self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE).then(c=>c.addAll(APP_SHELL)))});
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith('nr-bizpro-shell-')&&k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
